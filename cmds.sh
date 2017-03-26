822bcb4c9b82fa576b8ff4d786262b93a615f8d3
Viren Nadkarni <viren@outlook.com>
2017-03-26 15:30:33 +0530

    Add cmds.sh

---
+++ cmds.sh
@@ -0,0 +1,13 @@
+# scan image
+scanimage --resolution 300 --mode Color --device pixma > out.pnm
+pnmtojpeg out.pnm > out.jpeg
+
+# decrypt pdf
+qpdf -decrypt -password=EJDPS2603K in.pdf out.pdf
+
+# delete garbage metadata
+find . -iname thumbs.db -delete \
+    -o -iname desktop.ini -delete \
+    -o -iname .ds_store -delete \
+    -o -iname folder.jp*g -delete \
+    -o -iname albumart*.jp*g -delete
