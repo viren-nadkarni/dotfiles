bb2d1f5468576c6399830c89070d0947e15cb77b
Viren Nadkarni <viren@outlook.com>
2017-12-01 23:42:34 +0100

    Amendmends

--- cmds.sh
+++ cmds.sh
@@ -10,8 +10,8 @@ find . -iname thumbs.db -delete -o -iname desktop.ini -delete -o -iname .ds_stor
 
 # backup
 # trailing slashes are REQUIRED
-rsync -hav --delete-after --progress -n /data/music/ /media/viren/ntfs-passport/music/
-rsync -hav --progress -n /data/photos/ /media/viren/ntfs-passport/photos/
+rsync --human-readable --archive --verbose --delete-after --progress --dry-run /data/music/ /media/viren/ntfs-passport/music/
+rsync --human-readable --archive --verbose --progress --dry-run --progress -n /data/photos/ /media/viren/ntfs-passport/photos/
 
 # convert to gif
 ffmpeg -i input -pix_fmt rgb8 output
