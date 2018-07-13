8e4ef25383b5042f0de6788489dfa7fc695f08b9
Viren Nadkarni <viren.nadkarni@gmail.com>
2018-07-13 17:45:20 +0200

    Add i3 modified blocks

--- chromebook/startup.sh
+++ chromebook/startup.sh
@@ -6,20 +6,20 @@ libinput-gestures-setup start
 # remap caps to ctrl
 setxkbmap -option ctrl:nocaps
 
-# remap super to ctrl
-xmodmap -e "clear Control"
-xmodmap -e "clear Mod4"
-xmodmap -e "keysym Super_L = Control_L"\
-xmodmap -e "add Control = Control_L"
-xmodmap -e "add Mod4 = Super_L Super_R"
-
 # make the uk layout a bit more tolerable
 xmodmap -e 'keycode 51=Return'
 xmodmap -e 'keycode 36=backslash bar'
 xmodmap -e 'keycode 94=Shift_L'
 
+# remap super to ctrl
+xmodmap -e "clear Control"
+xmodmap -e "clear Mod4"
+xmodmap -e "keysym Super_L = Control_L"
+xmodmap -e "add Control = Control_L"
+xmodmap -e "add Mod4 = Super_L Super_R"
+
 # touchpad tweaks
-xinput set-button-map 11 1 2 3 4 5
-xinput set-prop 11 275 1
+xinput set-button-map 14 1 2 3 4 5
+xinput set-prop 14 275 1
 
 #export XDG_SESSION_TYPE="${XDG_SESSION_TYPE:-"x11"}"
